0d166d1e4350a7f0d72d9fc8a6fd4f2291973486
Iris-Smok <irissmok11@gmail.com>
2022-02-04 10:37:07 +0000

    Caled API with GET request, key status displayed in a modal, add basic errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
 const API_KEY = "bnFclat7BDkMzdXZ1OVUByow8tI";
-const API_URL = "https://ci-jshint.herokuapp.com/";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+    const response = await fetch(queryString);
+    const data = await response.json();
+    if (response.ok) {
+        displayStatus(data)
+    } else {
+        throw new Error(data.error);
+    }
+};
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
