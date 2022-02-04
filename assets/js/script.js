8636e28001cb06fb239cab02ce6dc9916c99859f
Iris-Smok <irissmok11@gmail.com>
2022-02-04 10:04:30 +0000

    Add const variables for API_KEY,API_URL and modal

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "bnFclat7BDkMzdXZ1OVUByow8tI";
+const API_URL = "https://ci-jshint.herokuapp.com/";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
